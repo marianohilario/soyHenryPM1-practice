35ce31ad3865e622119ac743b45617457a6a3f9e
Mariano <marianohilario@gmail.com>
2024-04-18 01:14:51 -0300

    First instance of Repository

--- scripts/index.js
+++ scripts/index.js
@@ -1,25 +1,25 @@
-const themeSwitch = document.querySelector(".header-switch");
+const themeSwitch = document.querySelector('.header-switch');
 
-themeSwitch.addEventListener("click", (e) => {
-  themeSwitch.classList.toggle("active");
-  document.body.classList.toggle("dark-mode-variables");
+themeSwitch.addEventListener('click', (e) => {
+  themeSwitch.classList.toggle('active');
+  document.body.classList.toggle('dark-mode-variables');
 });
 
 
-const inputs = document.querySelectorAll("input");
+const inputs = document.querySelectorAll('input');
 
 inputs.forEach((input) => {
   input.onfocus = () => {
-    input.previousElementSibling.classList.add("top", "focus");
-    input.parentNode.classList.add("focus");
+    input.previousElementSibling.classList.add('top', 'focus');
+    input.parentNode.classList.add('focus');
   };
   input.onblur = () => {
     input.value = input.value.trim();
     if (input.value.trim().length === 0) {
-      input.previousElementSibling.classList.remove("top");
+      input.previousElementSibling.classList.remove('top');
     }
-    input.previousElementSibling.classList.remove("focus");
-    input.parentNode.classList.remove("focus");
+    input.previousElementSibling.classList.remove('focus');
+    input.parentNode.classList.remove('focus');
   };
 });
 
@@ -27,28 +27,30 @@ inputs.forEach((input) => {
 const button = document.getElementById('button')
 
 button.addEventListener('click', () => {
-  for(let i = 0; i < 50; i++) {
-    const spark = document.createElement('i')
-    const form = document.getElementById('button')
-    
-    // random position
-    const x = (Math.random() - 0.5) * window.innerWidth
-    const y = (Math.random() - 0.5) * window.innerHeight
-    spark.style.setProperty('--x', x + 'px')
-    spark.style.setProperty('--y', y + 'px')
+  if (activity.value.trim() !== '' && detail.value.trim() !== '' && imgUrl.value.trim() !== '') {
+    for(let i = 0; i < 50; i++) {
+      const spark = document.createElement('i')
+      const form = document.getElementById('button')
+      
+      // random position
+      const x = (Math.random() - 0.5) * window.innerWidth
+      const y = (Math.random() - 0.5) * window.innerHeight
+      spark.style.setProperty('--x', x + 'px')
+      spark.style.setProperty('--y', y + 'px')
 
-    // random size
-    const randomSize = Math.random() * 8 + 2;
-    spark.style.width = randomSize + 'px';
-    spark.style.height = randomSize + 'px';
+      // random size
+      const randomSize = Math.random() * 8 + 2;
+      spark.style.width = randomSize + 'px';
+      spark.style.height = randomSize + 'px';
 
-    form.append(spark)
-    spark.classList.add('spark')
+      form.append(spark)
+      spark.classList.add('spark')
 
 
-    setTimeout(() => {
-      spark.remove()
-    }, 2000);
+      setTimeout(() => {
+        spark.remove()
+      }, 2000);
+    }
   }
 })
 
@@ -83,4 +85,26 @@ class Repository {
     })
   }
 }
+
+
+const repo = new Repository();
+
+const handler = () => {
+  const activity = document.getElementById('activity')
+  const detail = document.getElementById('detail')
+  const imgUrl = document.getElementById('imgUrl')
+  
+  if (activity.value.trim() === '' || detail.value.trim() === '' || imgUrl.value.trim() === '') {
+    alert('Por favor rellene todos los campos');
+  } else {
+    repo.createActivity(activity.value, detail.value, imgUrl.value);
+    activity.value = '';
+    detail.value = '';
+    imgUrl.value = '';
+    console.log(repo);
+  }
+}
+
+const boton = document.getElementById('boton')
+boton.addEventListener('click', handler)
 
